ea863c07463fc923a809875b212873ede156a4f5
Daffa Hanif Rabbanee <daffa.hanif@iconpln.co.id>
2025-11-24 21:21:56 +0700

    fix typo

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -17,4 +17,4 @@ export const prisma =
     },
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; ini lib/prisma.ts w
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
