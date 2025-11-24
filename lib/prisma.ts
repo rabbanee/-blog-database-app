97235f23d5ef4b19d136c1cdc3ed3f7bdcbb57c7
Daffa Hanif Rabbanee <daffa.hanif@iconpln.co.id>
2025-11-24 21:20:21 +0700

    fix: prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import path from "path";
 
-const globalForPrisma =
-globalThis as unknown as {prisma: PrismaClient;};
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const dbPath = path.join(process.cwd(), "prisma/dev.db");
 
 export const prisma =
-globalForPrisma.prisma ?? new PrismaClient({
-  log: ["query"],
-});
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+    datasources: {
+      db: {
+        url: process.env.NODE_ENV === "production"
+             ? `file:${dbPath}`
+             : process.env.DATABASE_URL,
+      },
+    },
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; ini lib/prisma.ts w
